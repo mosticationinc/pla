62f969acd9d075afda00db10be72b8e27fc75114
Anuntachai Pratthanaphorn <netumpire@gmail.com>
2014-03-12 13:12:08 +0700

    Added .gitignore file for ingnore desktop.ini, added node.js' template file.

---
+++ docs/templates/nodejs_module_filename.js
@@ -0,0 +1,56 @@
+/**
+ * Module Name	: mostication.nodejs.[MODULE_NAME]
+ * Copyright by	: Mostication Inc. and Most Vending Co.,Ltd.
+ * Author		: [YOUR_NAME]
+ * Created Date	: [CREATE_DATE]
+ * Description	: [MODULE_DESCRIPTION]
+ */
+
+
+/*
+ * Using
+ * 
+ * Import this module in your node.js file:
+ * var [MODULE_NAME]Module = require("./assets/libs/mostication/nodejs/[MODULE_NAME].js");		//	refer to package path
+ * var [MODULE_NAME] = mostication.nodejs.[MODULE_NAME]		//	make shortened package name for using (optional)
+ * 
+ * [EXAMPLE]
+ * 
+ */
+
+
+/*
+ * ---------------------------
+ * Define module
+ * ---------------------------
+ */
+if(typeof(mostication)=="undefined") mostication={};
+if(typeof(mostication.nodejs)=="undefined") mostication.nodejs={};
+if(typeof(mostication.nodejs.[MODULE_NAME])=="undefined") mostication.nodejs.[MODULE_NAME]={};
+
+
+/*
+ * ---------------------------
+ * Global Setting
+ * ---------------------------
+ */	
+
+
+
+/*
+ * ---------------------------
+ * Define Functions / Methods
+ * ---------------------------
+ */
+
+//	[FUNCTION_DESCRIPTION]
+//
+//	Parameters:
+//		- ([DATATYPE1]) [PARAM1] = [PARAM1_DESCRIPTION]
+//		- ([DATATYPE2]) [PARAM2] = [PARAM2_DESCRIPTION]
+//	Return:
+//		- ([RETURNED_DATATYPE]) = [RETURNED_DESCRIPTION]
+mostication.nodejs.[MODULE_NAME].[FUNCTION_NAME] = function([PARAM1], [PARAM2]){
+	
+	
+};
