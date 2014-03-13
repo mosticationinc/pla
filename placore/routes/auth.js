09248feea95b941a5148f12947f7e13461ff8a74
kurikce <kurik.ce@gmail.com>
2014-03-13 18:20:15 +0700

    Update auth.js

--- placore/routes/auth.js
+++ placore/routes/auth.js
@@ -1,3 +1,43 @@
-var passport = = require("passport");
-var localStrategy = require('passport-local').Strategy;
-var facebookStrategy = require('passport-facebook').Strategy;
+var async = require('async');
+var passport = require('passport');
+var local_strategy = require('passport-local').Strategy;
+var facebook_strategy = require('passport-facebook').Strategy;
+
+var provider = require('../provider');
+
+exports.login = function (req, res) {
+    res.redirect('/payment');
+};
+
+exports.logout = function (req, res) {
+    res.logout();
+    res.end('');
+};
+
+exports.local_strategy = function () {
+    return new local_strategy(function (user, password, done) {
+        do_login(user, password, done);
+    });
+}
+
+exports.is_authenticated = function (req, res, next) {
+    if (req.isAuthenticated()) {
+        next();
+    } else {
+        res.send(401, { error: 'Unauthorized' });
+    }
+}
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (id, done) {
+    done(null, id);
+});
+
+function do_login(email, password, done) {
+    done(null, {
+        id: 1,
+    });
+}
